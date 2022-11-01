baad551433021a62d98992271486a82b47299e9d
ahijazy <94984907+ahijazy@users.noreply.github.com>
2022-11-01 11:56:01 +0100

    Update CreatePredictionAnalysisDetails.R
    model settings list reduced to only run lasoo logistic regression

--- extras/CreatePredictionAnalysisDetails.R
+++ extras/CreatePredictionAnalysisDetails.R
@@ -27,22 +27,7 @@
 
 createAnalysesDetails <- function(workFolder) {
    # 1) ADD MODELS you want
-  modelSettingList <- list(setAdaBoost(nEstimators = c(10,50,100), learningRate = c(0.5,0.9,1)),
-                           setLassoLogisticRegression(),
-                           setGradientBoostingMachine(), 
-                           setCIReNN(), 
-                           setCNNTorch(), 
-                           setCovNN(), 
-                           setCovNN2(), 
-                           setDecisionTree(), 
-                           setDeepNN(), 
-                           setKNN(), 
-                        setLRTorch(), 
-                        setMLP(), 
-                        setMLPTorch(), 
-                        setNaiveBayes(), 
-                        setRandomForest(), 
-                        setRNNTorch())
+  modelSettingList <- list(setLassoLogisticRegression())
   
   # 2) ADD POPULATIONS you want
   pop1 <- createStudyPopulationSettings(riskWindowStart = 1, 
